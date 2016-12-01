cf826a39f19530dd35d06220a5ead46d942df0c8
Raymon Been <raymonbeenxbox@gmail.com>
2016-12-01 13:38:01 +0100

    Added a test project

---
+++ Test/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Baker's CallOfDuty Tools")> 
+<Assembly: AssemblyDescription(".NET Library containing tools for developing Call Of Duty content")> 
+<Assembly: AssemblyCompany("Sergeant_Baker")> 
+<Assembly: AssemblyProduct("Baker's Game Development Tools")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dcb3f1ee-0cef-40b5-9e99-d5597cab6e40")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
